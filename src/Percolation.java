d6575c06cf43c7532439e164d6f37228ed698686
Correct Name <tambad.samarth@gmail.com>
2018-05-23 23:45:23 +0530

    Percolation and PercolationStats code completed with backwash solution implementation.

--- src/Percolation.java
+++ src/Percolation.java
@@ -17,13 +17,14 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
 
     private WeightedQuickUnionUF grid;
+    private WeightedQuickUnionUF fullness;
     private boolean[] isOpen;
     private int dimension;
     private int numOfOpenSites;
     private int virtualTop;
     private int virtualBottom;
 
-    private Percolation (int n) {    // create n-by-n grid, with all sites blocked
+    public Percolation (int n) {    // create n-by-n grid, with all sites blocked
         if(n > 0) {
             dimension = n;
             numOfOpenSites = 0;
@@ -32,32 +33,64 @@ public class Percolation {
             int numOfSites = n * n;
             isOpen = new boolean[numOfSites];
             grid = new WeightedQuickUnionUF(numOfSites + 2);
+            fullness = new WeightedQuickUnionUF(numOfSites + 1);
         } else {
             throw new java.lang.IllegalArgumentException("The value of n must be > 0");
         }
     }
 
     public void open(int row, int col){     // open site (row, col) if it is not open already
-
+        validate(row, col);
+        int currentSite = indexTo1D(row, col);
+        if(!isOpen(row, col)){
+            isOpen[currentSite] = true;
+            numOfOpenSites++;
+            if(row == 1){
+                grid.union(currentSite, virtualTop);
+                fullness.union(currentSite, virtualTop);
+            }
+            if(row == dimension){
+                grid.union(currentSite, virtualBottom);
+            }
+            if(row > 1 && isOpen((row - 1), col)){
+                grid.union(currentSite, indexTo1D((row - 1), col));
+                fullness.union(currentSite, indexTo1D((row - 1), col));
+            }
+            if(row < dimension && isOpen((row + 1), col)){
+                grid.union(currentSite, indexTo1D((row + 1), col));
+                fullness.union(currentSite, indexTo1D((row + 1), col));
+            }
+            if(col > 1 && isOpen(row, (col - 1))){
+                grid.union(currentSite, indexTo1D(row, (col - 1)));
+                fullness.union(currentSite, indexTo1D(row, (col - 1)));
+            }
+            if(col < dimension && isOpen(row, (col + 1))){
+                grid.union(currentSite, indexTo1D(row, (col + 1)));
+                fullness.union(currentSite, indexTo1D(row, (col + 1)));
+            }
+        }
     }
 
     public boolean isOpen(int row, int col){    // is site (row, col) open?
-        return false;
+        validate(row, col);
+        return isOpen[indexTo1D(row, col)];
     }
 
     public boolean isFull(int row, int col){    // is site (row, col) full?
-        return false;
+        validate(row, col);
+        return fullness.connected(indexTo1D(row, col), virtualTop);
     }
 
     public int numberOfOpenSites(){     // number of open sites
-        return 0;
+        return numOfOpenSites;
     }
 
     public boolean percolates(){    // does the system percolate?
-        return false;
+        return grid.connected(virtualTop, virtualBottom);
     }
 
     private int indexTo1D(int row, int col){    //convert index from 2D (row, col) to 1D for WeightedQuickUnionUF implementation
+        validate(row, col);
         row -= 1;
         col -= 1;
         return (row * dimension) + col;
