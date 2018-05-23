6a80ac17cf2fb3523d43e7b9d6f78da60937cc12
Correct Name <tambad.samarth@gmail.com>
2018-05-23 18:49:26 +0530

    basic code complete

---
+++ src/Percolation.java
@@ -0,0 +1,78 @@
+/*------------------------------------------------------------------------------------------------------------------
+    Author: Samarth Tambad
+    Class Name: Percolation.java
+    Dependencies: WeightedQuickUnionUF (algs4.jar)
+    Compile: javac Percolation.java
+    Run: java Percolation.java
+    Description: This class is a model for percolation object. It creates a nxn grid of sites.
+    All sites are closed by default. The user can choose to open a site at a given location (row, col).
+    The user can also choose to check if the current model percolates i.e. if there is a path b/w any sites at the
+    topmost and bottommost layer. The user can also check if the site is open and is the site is full i.e. this site
+    is connected to any site at the topmost layer.
+    The problem of backwash sometimes presents itself. This problem is also tacked in this code.
+------------------------------------------------------------------------------------------------------------------*/
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+    private WeightedQuickUnionUF grid;
+    private boolean[] isOpen;
+    private int dimension;
+    private int numOfOpenSites;
+    private int virtualTop;
+    private int virtualBottom;
+
+    private Percolation (int n) {    // create n-by-n grid, with all sites blocked
+        if(n > 0) {
+            dimension = n;
+            numOfOpenSites = 0;
+            virtualTop = indexTo1D(n, n) + 1;
+            virtualBottom = indexTo1D(n, n) + 2;
+            int numOfSites = n * n;
+            isOpen = new boolean[numOfSites];
+            grid = new WeightedQuickUnionUF(numOfSites + 2);
+        } else {
+            throw new java.lang.IllegalArgumentException("The value of n must be > 0");
+        }
+    }
+
+    public void open(int row, int col){     // open site (row, col) if it is not open already
+
+    }
+
+    public boolean isOpen(int row, int col){    // is site (row, col) open?
+        return false;
+    }
+
+    public boolean isFull(int row, int col){    // is site (row, col) full?
+        return false;
+    }
+
+    public int numberOfOpenSites(){     // number of open sites
+        return 0;
+    }
+
+    public boolean percolates(){    // does the system percolate?
+        return false;
+    }
+
+    private int indexTo1D(int row, int col){    //convert index from 2D (row, col) to 1D for WeightedQuickUnionUF implementation
+        row -= 1;
+        col -= 1;
+        return (row * dimension) + col;
+    }
+
+    private boolean isValid(int row, int col){
+        row -= 1;
+        col -= 1;
+        return ((row >= 0 && row < dimension) && (col >= 0 && col < dimension));
+    }
+
+    private void validate(int row, int col){
+        if(!isValid(row, col)){
+            throw new IllegalArgumentException("Not valid values for (row, col)");
+        }
+    }
+
+}
