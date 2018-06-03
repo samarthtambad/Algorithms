1e9fa554115640ee1b57d6376a7e0c1656b9542e
Correct Name <tambad.samarth@gmail.com>
2018-06-03 21:59:57 +0530

    minor changes

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,23 +1,37 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import edu.princeton.cs.algs4.*;
 
 public class FastCollinearPoints {
 
+    private List<LineSegment> segments;
+
     public FastCollinearPoints(Point[] points){     // finds all line segments containing 4 or more points
         if(points == null) throw new IllegalArgumentException("Not a valid argument input");
         checkDuplicatePoints(points);
+        segments = new ArrayList<>();
         Point[] aux = Arrays.copyOf(points, points.length);
-        for (Point origin:points){
+        for (int i = 0;i < points.length; i++){
+            Point origin = points[i];
             Arrays.sort(aux, origin.slopeOrder());
+            int j = i;
+            while(aux[i + 3] != null){
+                if((origin.slopeTo(aux[i+1]) == origin.slopeTo(aux[i+2])) && (origin.slopeTo(aux[i+2]) == origin.slopeTo(aux[i+3]))){
+                    origin.drawTo(aux[i+3]);
+                    segments.add(new LineSegment(origin, aux[i+3]));
+                }
+            }
         }
     }
 
     public int numberOfSegments(){      // the number of line segments
-        return 0;
+        return segments.size();
     }
 
     public LineSegment[] segments(){    // the line segments
-        return null;
+        return segments.toArray(new LineSegment[segments.size()]);
     }
 
     private void checkDuplicatePoints(Point[] points) {
@@ -32,7 +46,7 @@ public class FastCollinearPoints {
 
     public static void main(String[] args){
         // read the n points from a file
-        In in = new In(args[0]);
+        In in = new In("input40.txt");
         int n = in.readInt();
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
@@ -52,6 +66,7 @@ public class FastCollinearPoints {
 
         // print and draw the line segments
         FastCollinearPoints collinear = new FastCollinearPoints(points);
+        System.out.println(collinear.numberOfSegments());
         for (LineSegment segment : collinear.segments()) {
             StdOut.println(segment);
             segment.draw();
