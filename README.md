317dde69f26051d2e5d013a93f4d5835065c4351
Samarth Tambad <tambad.samarth@gmail.com>
2019-02-24 12:41:25 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# Algorithms
-Implementation of programming assignments for Algorithms course on Coursera (Princeton University).
+### Algorithms
+Implementation of various algorithms I have come across.
 
 
-### List of Implementations (so far)
+##### List of Implementations (so far)
+
 * Percolation
 * Deques and Randomized Queues
+* Heap Sort
+* Sorts - Insertion, Bubble
+* Stock Profit Problem
--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,6 @@
 
 public class Test {
-
     public static void main(String args[]){
 
-        int[] A = {5, 4, 7, 8, 3, 2, 9, 1};
-        System.out.println(java.util.Arrays.toString(A));
-        PriorityQueue pq = new PriorityQueue(A);
-        System.out.println(java.util.Arrays.toString(A));
-        System.out.println(pq.heapMaximum(A));
-        pq.heapExtractMax(A);
-        System.out.println(java.util.Arrays.toString(A));
-        pq.maxHeapInsert(A, 15);
-        System.out.println(java.util.Arrays.toString(A));
     }
-
 }
