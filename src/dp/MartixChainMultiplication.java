2a25971435b9bd06b9099257698179fe50a828c0
Samarth Tambad <tambad.samarth@gmail.com>
2019-04-09 07:50:19 -0400

    minor

--- src/dp/MartixChainMultiplication.java
+++ src/dp/MartixChainMultiplication.java
@@ -32,16 +32,23 @@ public class MartixChainMultiplication {
         int n = p.length - 1;
         int[][] m = new int[n][n];
         int[][]s = new int[n][n];
+        boolean isFirst = true;
 
         for(int l = 1; l < n; l++){
             for(int i = 0; i < n - l + 1; i++){
                 int j = i + l - 1;
-                m[i][j] = 65535;
-                for(int k = i; k < j-1; k++){
-                    int q = m[i][k] + m[k+1][j] + (p[i] * p[j + 1] * p[k + 1]);
-                    if(q < m[i][j]){
+                for(int k = i - 1; k < j-1; k++){
+                    int q = m[i][k] + m[k+1][j] + (p[i] * p[k + 1] * p[j + 1]);
+                    if(isFirst){
                         m[i][j] = q;
                         s[i][j] = k;
+                        isFirst = false;
+                        System.out.print(q);
+                    }
+                    else if(q < m[i][j]){
+                        m[i][j] = q;
+                        s[i][j] = k;
+                        System.out.print(q);
                     }
                 }
             }
